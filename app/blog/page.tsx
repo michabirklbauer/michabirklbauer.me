42856c0f01b5e4cc55e375875224eb5629bce0fd
Micha Birklbauer <micha.birklbauer@gmail.com>
2024-09-08 22:26:31 +0200

    update post order

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -9,9 +9,9 @@ import { Eye } from "lucide-react";
 export const revalidate = 60;
 export default async function BlogPage() {
 
-  const featured = allBlogs.find((blog) => blog.slug === "girlfriend")!;
-  const top2 = allBlogs.find((blog) => blog.slug === "ai")!;
-  const top3 = allBlogs.find((blog) => blog.slug === "privacy")!;
+  const featured = allBlogs.find((blog) => blog.slug === "matrixxlms")!;
+  const top2 = allBlogs.find((blog) => blog.slug === "girlfriend")!;
+  const top3 = allBlogs.find((blog) => blog.slug === "ai")!;
   const sorted = allBlogs
     .filter((b) => b.published)
     .filter(
